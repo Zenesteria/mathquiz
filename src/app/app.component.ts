4cb4eedf874938788064675e24caefa0e0a9aff8
The Zenith <abdurrahman.aderinto@gmail.com>
2024-08-25 07:07:31 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgxSemanticModule } from 'ngx-semantic';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [ReactiveFormsModule, NgxSemanticModule],
 })
 export class AppComponent {
-  title = 'mathquiz';
+  num1!: number;
+  num2!: number;
+  correctAnswer!: number;
+  correctAttempts: number = 0;
+  incorrectAttempts: number = 0;
+  formGroup!: FormGroup;
+
+  constructor() {
+    this.formGroup = new FormGroup({
+      userAnswer: new FormControl(0, [Validators.required]),
+    });
+    this.generateQuestion();
+  }
+
+  generateQuestion() {
+    this.formGroup.value.userAnswer = null;
+    this.num1 = Math.floor(Math.random() * 10) + 1;
+    this.num2 = Math.floor(Math.random() * 10) + 1;
+    this.correctAnswer = this.num1 + this.num2;
+  }
+
+  checkAnswer() {
+    if (this.formGroup.value.userAnswer === this.correctAnswer) {
+      this.correctAttempts++;
+    } else {
+      this.incorrectAttempts++;
+    }
+    this.generateQuestion();
+  }
 }
